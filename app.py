0e07a0caefaf17a9a4d2d52434877ab762d10339
alexanderdx <alexandrugabriel.dumea@gmail.com>
2022-01-28 21:54:11 +0200

    Create CRUD operations for Hub model

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import request
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -11,20 +13,40 @@ class Hub(db.Model):
     user_number = db.Column(db.Integer)
 
     def __repr__(self):
-        return f"{self.id}, {self.name}, {self.user_number}"
-
+        return f'{self.id}, {self.name}, {self.user_number}'
 
 @app.route('/')
 def hello():
     return 'Hello'
-
 
 @app.route('/hubs')
 def get_hubs():
     hubs = Hub.query.all()
     output = []
     for hub in hubs:
-        hub_data = {"id": hub.id, "name": hub.name, "user_nr": hub.user_number}
+        hub_data = {'id': hub.id, 'name': hub.name, 'user_nr': hub.user_number}
         output.append(hub_data)
 
-    return {"hubs": output}
+    return {'hubs': output}
+
+@app.route('/hubs/<id>')
+def get_hub_by_id(id):
+    hub = Hub.query.get_or_404(id)
+    return {'id': hub.id, 'name': hub.name}
+
+@app.route('/hubs', methods=['POST'])
+def add_hub():
+    hub = Hub(id=request.json['id'], name=request.json['name'], user_number=request.json['user_number'])
+    db.session.add(hub)
+    db.session.commit()
+    return {'id': hub.id, 'name': hub.name}
+
+@app.route('/hubs/<id>', methods=['DELETE'])
+def delete_hub(id):
+    hub = Hub.query.get(id)
+    if hub is None:
+        return {"error": "Hub not found."}
+
+    db.session.delete(hub)
+    db.session.commit()
+    return {"message": "Hub deleted successfully."}
