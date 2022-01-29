f4a758e1728599ff3d7550fe4ccc074378dba9b5
Alexandru Dumea <alexandrugabriel.dumea@gmail.com>
2022-01-29 21:53:45 +0200

    Supress Flask warning. See https://stackoverflow.com/questions/33738467/how-do-i-know-if-i-can-disable-sqlalchemy-track-modifications

--- app.py
+++ app.py
@@ -4,20 +4,25 @@ from flask import request
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Supress flask warning
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
 db = SQLAlchemy(app)
 
+
 class Hub(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     user_number = db.Column(db.Integer)
 
     def __repr__(self):
         return f'{self.id}, {self.name}, {self.user_number}'
 
+
 @app.route('/')
 def hello():
     return 'Hello'
+
 
 @app.route('/hubs')
 def get_hubs():
@@ -29,17 +34,21 @@ def get_hubs():
 
     return {'hubs': output}
 
+
 @app.route('/hubs/<id>')
 def get_hub_by_id(id):
     hub = Hub.query.get_or_404(id)
     return {'id': hub.id, 'name': hub.name}
 
+
 @app.route('/hubs', methods=['POST'])
 def add_hub():
-    hub = Hub(id=request.json['id'], name=request.json['name'], user_number=request.json['user_number'])
+    hub = Hub(id=request.json['id'], name=request.json['name'],
+              user_number=request.json['user_number'])
     db.session.add(hub)
     db.session.commit()
     return {'id': hub.id, 'name': hub.name}
+
 
 @app.route('/hubs/<id>', methods=['DELETE'])
 def delete_hub(id):
