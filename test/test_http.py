500e89a9fa44694d89abbc2619faa440916568cd
ValeanuCosminTeodor <62661276+ValeanuCosminTeodor@users.noreply.github.com>
2022-02-01 21:41:17 +0200

    IP-8 Add unit test for user

--- test/test_http.py
+++ test/test_http.py
@@ -73,7 +73,7 @@ def test_minihub_update_description(client):
     assert request.status_code == 200
     assert payload['description'] == result['description']
 
-def test_minihub_update_description(client):
+def test_minihub_update_volume(client):
     # Create a minihub
     payload = {'description': 'Test description', 'connected_user_id': 0, 'volume': 55}
     request = client.post('/minihub', json=payload, follow_redirects=True)
@@ -96,4 +96,64 @@ def test_minihub_deletion(client):
     payload = {'action': 'change_volume', 'volume': 100}
     request = client.delete('/minihub/{}'.format(hub_id), json=payload, follow_redirects=True)
     
+    assert request.status_code == 200
+
+
+def test_user_get(client):
+    request = client.get('/user')
+    assert request.status_code == 200
+
+
+def test_user_post(client):
+    payload = {'name': 'Jon Doe', 'output': 'Test output', 'room': 'Test room'}
+
+    request = client.post('/user', json=payload, follow_redirects=True)
+
+    result = json.loads(request.data.decode())
+
+    assert request.status_code == 200
+    assert payload['name'] == result['name']
+    assert payload['output'] == result['output']
+    assert payload['room'] == result['room']
+
+
+def test_user_update_room(client):
+    # Create a user
+    payload = {'name': 'Alice Cooper', 'output': 'Test output', 'room': 'Test room'}
+    request = client.post('/user', json=payload, follow_redirects=True)
+    user_id = json.loads(request.data.decode())['id']
+
+    payload = {'action': 'change_room', 'room': 'New room'}
+    request = client.patch('/user/{}'.format(user_id), json=payload, follow_redirects=True)
+
+    result = json.loads(request.data.decode())
+
+    assert request.status_code == 200
+    assert payload['room'] == result['room']
+
+
+def test_user_update_output(client):
+    # Create a user
+    payload = {'name': 'Karl Marx', 'output': 'Test output', 'room': 'Test room'}
+    request = client.post('/user', json=payload, follow_redirects=True)
+    user_id = json.loads(request.data.decode())['id']
+
+    payload = {'action': 'change_output', 'output': 'New output'}
+    request = client.patch('/user/{}'.format(user_id), json=payload, follow_redirects=True)
+
+    result = json.loads(request.data.decode())
+
+    assert request.status_code == 200
+    assert payload['output'] == result['output']
+
+
+def test_user_deletion(client):
+    # Create a user
+    payload = {'name': 'Stresu computers', 'output': 'Test output', 'room': 'Test room'}
+    request = client.post('/user', json=payload, follow_redirects=True)
+    user_id = json.loads(request.data.decode())['id']
+
+    payload = {'action': 'change_output', 'output': 'New output'}
+    request = client.delete('/user/{}'.format(user_id), json=payload, follow_redirects=True)
+
     assert request.status_code == 200
